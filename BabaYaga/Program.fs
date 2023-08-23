7db419a266d815931df19db531a66552c22cb4e0
cap coldman <moctopus@outlook.com>
2023-08-23 13:48:34 -0400

    adding basic color features, using env variables, updating gitignore, and beginning work on updating line reading logic

--- BabaYaga/Program.fs
+++ BabaYaga/Program.fs
@@ -2,10 +2,18 @@
 open System.IO
 open System.Net.Sockets
 
-let server = "####"
-let port  = 6667
-let channel = "#####"
-let nick = "####"
+type ChannelMessage = 
+    { UserInfo: string; Channel: string; Message: string }
+
+type ConsoleMessage = 
+    | Command of string
+    | Input of string
+    | Output of string
+
+let server = Environment.GetEnvironmentVariable("SERVER")
+let port  = int (Environment.GetEnvironmentVariable("PORT"))
+let channel = Environment.GetEnvironmentVariable("CHANNEL")
+let nick = Environment.GetEnvironmentVariable("NICK")
 
 Console.ForegroundColor <- ConsoleColor.DarkRed
 
@@ -19,6 +27,19 @@ irc_writer.AutoFlush <- true
 irc_writer.WriteLine(sprintf "NICK %s\r\n" nick)
 irc_writer.WriteLine(sprintf "USER %s %s %s %s\r\n" nick nick nick nick)
 
+let writeText (input:ConsoleMessage) = 
+    match input with
+    | Command message -> 
+        Console.ForegroundColor <- ConsoleColor.Red
+        Console.WriteLine(message)
+    | Input message -> 
+        Console.ForegroundColor <- ConsoleColor.DarkRed
+        Console.WriteLine(message)
+    | Output message -> 
+        Console.ForegroundColor <- ConsoleColor.DarkYellow
+        Console.WriteLine(message)
+    
+
 let irc_ping (writer : StreamWriter) (line:string) =
     let cookie = (line.Split ':')[1]
     let output = sprintf "PONG :%s %s\r\n" cookie server
@@ -30,8 +51,9 @@ let joinChannel (writer : StreamWriter) =
     irc_writer.WriteLine(output)
     writer.WriteLine(output)
 
-let irc_privmsg (writer : StreamWriter) (phrase : string) =
-    writer.WriteLine(sprintf "PRIVMSG %s %s\r\n" channel phrase)
+let irc_privmsg (writer : StreamWriter) (message : string) =
+    writer.WriteLine(sprintf "PRIVMSG %s %s\r\n" channel message)
+    writeText <| Output message
 
 let (|Contains|_|) (ircResponse:string) (serverInput:string) = 
     if serverInput.Contains(ircResponse) then
@@ -39,17 +61,37 @@ let (|Contains|_|) (ircResponse:string) (serverInput:string) =
     else
         None
 
-let (|Command|_|) (command:string) (serverInput:string) =
+let (|Commandz|_|) (command:string) (serverInput:string) =
     if serverInput.Substring(serverInput.Substring(1).IndexOf(":") + 2).StartsWith(command) then
+        writeText <| Command serverInput
         Some(command)
     else
         None
 
+let getSomeInfo (line:string) = 
+    let split = line.Split(':')
+    
+    if split.Length < 3 then
+        None
+    else
+        let messageDetails = split[1].Split(' ')
+        if messageDetails.Length < 4 then
+            None
+        else
+            Some({ UserInfo = split[1]; Channel = messageDetails[2]; Message = split[2]})
+    
+
 while(irc_reader.EndOfStream = false) do
     let line = irc_reader.ReadLine()
 
+    let x = getSomeInfo line
+
+    //match x with
+    //| Some a -> Console.WriteLine(line)
+    //| _ -> Console.WriteLine(line)
+
     match line with
-    | Command "!date" input -> irc_privmsg irc_writer (sprintf "%A" System.DateTime.Now)
+    | Commandz "!date" input -> irc_privmsg irc_writer (sprintf "%A" System.DateTime.Now)
     | Contains "PING" input -> irc_ping irc_writer line
     | Contains "+iwx" input -> joinChannel irc_writer
-    | _ -> Console.WriteLine(line)
+    | _ -> writeText <| Input line
