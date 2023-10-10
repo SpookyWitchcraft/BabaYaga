5029e1a3e5e259b752b5081d327129ffb333b03d
cap coldman <moctopus@outlook.com>
2023-10-10 14:45:19 -0400

    continuing refactoring progress thinking this should simplify things

---
+++ BabaYaga/Modules/TcpClientProxy.fs
@@ -0,0 +1,33 @@
+﻿module TcpClientProxy
+
+open Modules.Environment
+
+open System.Net.Sockets
+open System.IO
+
+let server = getEnvironmentVariables["SERVER"]
+let port  = int (getEnvironmentVariables["PORT"])
+
+let client = new TcpClient();
+client.Connect(server, port)
+
+let reader = new StreamReader(client.GetStream())
+let writer = new StreamWriter(client.GetStream())
+writer.AutoFlush <- true
+
+//writer(sprintf "NICK %s\r\n" nick)
+//writer(sprintf "USER %s 0 * %s\r\n" nick nick)
+
+let writeAsync (message:string) =
+    async {
+        let! _ = writer.WriteAsync(message) |> Async.AwaitTask
+
+        return ()
+    }
+
+let readAsync () = 
+    async {
+        let! line = reader.ReadLineAsync() |> Async.AwaitTask
+
+        return line
+    }
--- BabaYaga/Program.fs
+++ BabaYaga/Program.fs
@@ -16,52 +16,54 @@ let channel = getEnvironmentVariables["CHANNEL"]
 let nick = getEnvironmentVariables["NICK"]
 let password = getEnvironmentVariables["PASSWORD"]
 
-let initialState = 
-    let irc_client = new TcpClient();
-    irc_client.Connect(server, port)
+//let initialState = 
+//    let irc_client = new TcpClient();
+//    irc_client.Connect(server, port)
 
-    let irc_reader = new StreamReader(irc_client.GetStream())
-    let irc_writer = new StreamWriter(irc_client.GetStream())
-    irc_writer.AutoFlush <- true
-    { 
-        client = irc_client
-        reader = irc_reader
-        writer = irc_writer
-        question = None
-        rounds = 0
-        scores = new Dictionary<string, int>()
-        botState = Unidentified
-    }
+//    let irc_reader = new StreamReader(irc_client.GetStream())
+//    let irc_writer = new StreamWriter(irc_client.GetStream())
+//    irc_writer.AutoFlush <- true
+//    { 
+//        client = irc_client
+//        reader = irc_reader
+//        writer = irc_writer
+//        question = None
+//        rounds = 0
+//        scores = new Dictionary<string, int>()
+//        botState = Unidentified
+//    }
 
-let mutable state = initialState
+//let mutable state = initialState
 
-Console.ForegroundColor <- ConsoleColor.DarkRed
+//Console.ForegroundColor <- ConsoleColor.DarkRed
 
-state.writer.WriteLine(sprintf "NICK %s\r\n" nick)
-state.writer.WriteLine(sprintf "USER %s 0 * %s\r\n" nick nick)
+TcpClientProxy.writeAsync(sprintf "NICK %s\r\n" nick) |> Async.RunSynchronously
+TcpClientProxy.writeAsync(sprintf "USER %s 0 * %s\r\n" nick nick) |> Async.RunSynchronously
     
-let irc_ping (writer : StreamWriter) (line:string) =
+let irc_ping (line:string) =
     let cookie = (line.Split ':')[1]
     let output = sprintf "PONG :%s %s" cookie server
     writeText <| Input line
     writeText <| Output output
-    writer.WriteLine(output + "\r\n")
+    TcpClientProxy.writeAsync(output + "\r\n") |> Async.RunSynchronously
 
-let identify (writer : StreamWriter) (line:string) =
+let identify (line:string) =
     let output = sprintf "nickserv identify %s\r\n" password
     writeText <| Input line
-    writer.WriteLine(output)
+    TcpClientProxy.writeAsync(output) |> Async.RunSynchronously
 
-let joinChannel (writer : StreamWriter) (line:string) =
+let joinChannel (line:string) =
     let output = sprintf "JOIN %s" channel
     writeText <| Input line
     writeText <| Output output
-    writer.WriteLine(output + "\r\n")
+    TcpClientProxy.writeAsync(output + "\r\n") |> Async.RunSynchronously
 
-let irc_privmsg (input : string) (message : string) =
-    state.writer.WriteLine(sprintf "PRIVMSG %s %s\r\n" channel message)
-    writeText <| Command input
-    writeText <| Output message
+//let irc_privmsg (input : string) (message : string) =
+//    state.writer.WriteLine(sprintf "PRIVMSG %s %s\r\n" channel message)
+//    //this is the actual command someone typed
+//    writeText <| Command input
+//    //this is the actual output (shouldn't be in the same function)
+//    writeText <| Output message
 
 let getMessageInfo (line:string) = 
     let split = line.Split(':')
@@ -75,48 +77,48 @@ let getMessageInfo (line:string) =
         else
             Some({ UserInfo = split[1]; Channel = messageDetails[2]; Message = split[2]})
 
-let handleTriviaCommand (input:string) (triviaRounds:string) = 
-    let out = irc_privmsg input
+//let handleTriviaCommand (input:string) (triviaRounds:string) = 
+//    let out = irc_privmsg input
 
-    match state.question with
-        | None -> 
-            state <- { state with question = Trivia.Service.getTriviaQuestion(); rounds = int triviaRounds }
-            out <| Trivia.Service.questionOutput state.question
-        | _ -> ()
+//    match state.question with
+//        | None -> 
+//            state <- { state with question = Trivia.Service.getTriviaQuestion(); rounds = int triviaRounds }
+//            out <| Trivia.Service.questionOutput state.question
+//        | _ -> ()
 
-let handleCommand (input:string) (message:string) = 
-    let split = message.Split(' ', 2)
-    let command = split[0]
+//let handleCommand (input:string) (message:string) = 
+//    let split = message.Split(' ', 2)
+//    let command = split[0]
 
-    let out = irc_privmsg input
+//    let out = irc_privmsg input
 
-    match command with
-    | "!coinflip" -> out <| CoinFlip.Service.flip ()
-    | "!roll" -> out <| Roll.Service.getDice split[1]
-    | "!trivia" -> 
-        let l = split.Length
-        if l = 2 then
-            handleTriviaCommand input split[1]
-        else
-            handleTriviaCommand input "0"
-    | "!chatgpt" -> 
-        let answer = ChatGpt.Service.getGptAnswer split[1]
-        answer 
-        |> List.iter out
-    | "!marvel" -> out <| Marvel.Service.getMarvelCharacter split[1]
-    | "!report" -> out <| GitHub.Service.createIssue input split[1]
-    | _ -> out "command not found 👻"
+//    match command with
+//    | "!coinflip" -> out <| CoinFlip.Service.flip ()
+//    | "!roll" -> out <| Roll.Service.getDice split[1]
+//    | "!trivia" -> 
+//        let l = split.Length
+//        if l = 2 then
+//            handleTriviaCommand input split[1]
+//        else
+//            handleTriviaCommand input "0"
+//    | "!chatgpt" -> 
+//        let answer = ChatGpt.Service.getGptAnswer split[1]
+//        answer 
+//        |> List.iter out
+//    | "!marvel" -> out <| Marvel.Service.getMarvelCharacter split[1]
+//    | "!report" -> out <| GitHub.Service.createIssue input split[1]
+//    | _ -> out "command not found 👻"
 
-let timer = new Timer(
-          TimerCallback (fun _ -> Trivia.Service.checkQuestionStatus(&state)),
-          state,
-          0,
-          500
-        )
+//let timer = new Timer(
+//          TimerCallback (fun _ -> Trivia.Service.checkQuestionStatus(&state)),
+//          state,
+//          0,
+//          500
+//        )
 
 let identifyAndJoin (line:string) = 
-    identify state.writer line
-    joinChannel state.writer line
+    identify line
+    joinChannel line
 
 //clean up initial irc commands
 //set app to 'identified'
@@ -124,16 +126,17 @@ let identifyAndJoin (line:string) =
 //reuse http stuff
 //handle http codes better
 //handle timers better
-while(state.reader.EndOfStream = false) do
-    let line = state.reader.ReadLine()
+while(TcpClientProxy.reader.EndOfStream = false) do
+    let line = TcpClientProxy.readAsync() |> Async.RunSynchronously
 
     let messageInfo = getMessageInfo line
 
     match messageInfo with
-    | Some a -> (Trivia.Service.checkAnswer &state a.Message a.UserInfo)
+    | Some a -> //(Trivia.Service.checkAnswer &state a.Message a.UserInfo)
                 match a with
-                | y when a.Message.StartsWith("!") -> handleCommand line y.Message
-                | _ when a.Message.Contains("PING") -> irc_ping state.writer line
-                | _ when state.botState = Unidentified && a.Message.Contains("+iwx") -> identifyAndJoin line
+                //| y when a.Message.StartsWith("!") -> handleCommand line y.Message
+                | _ when a.Message.Contains("PING") -> irc_ping line
+                | _ when a.Message.Contains("+iwx") -> identifyAndJoin line
+                //| _ when state.botState = Unidentified && a.Message.Contains("+iwx") -> identifyAndJoin line
                 | _ -> writeText <| Input line
     | _ -> Console.WriteLine(line)
