cd623148fd63eee817ca65915bcf35d1a10eb60e
cap coldman <moctopus@outlook.com>
2023-12-14 13:57:57 -0500

    oh you know just being a little test goblin

---
+++ BabaYaga.Tests/CoinFlipTests.fs
@@ -0,0 +1,10 @@
+﻿module CoinFlipTests
+
+open Xunit
+open CoinFlip.Service
+
+[<Fact>]
+let ``flip should return heads or tails`` () =
+    let flipped = flip ()
+    let results = (flipped = "tails" || flipped = "heads")
+    Assert.True(results)
