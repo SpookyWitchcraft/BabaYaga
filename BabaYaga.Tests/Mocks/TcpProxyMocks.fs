9b7f17324ee5b3b5879434423dee9f4d81621820
cap coldman <moctopus@outlook.com>
2024-01-28 12:03:57 -0500

    going to need to rethink this again

---
+++ BabaYaga.Tests/Mocks/TcpProxyMocks.fs
@@ -0,0 +1,24 @@
+﻿module TcpProxyMocks
+
+open Application.Types
+open System.IO
+
+type TcpProxyMock() = 
+    let reader = new StreamReader(new MemoryStream())
+
+    interface ITcpProxy with
+        member _.Reader = reader
+        member _.WriteAsync (message:string) =
+            async {
+                ()
+            }
+
+        member _.ReadAsync() = 
+            async {
+                return "Mock"
+            }
+
+
+
+
+    
