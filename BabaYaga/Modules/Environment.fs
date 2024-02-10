547f77f208f97247e89897141b77c4a3d9ebcffd
cap coldman <moctopus@outlook.com>
2024-02-10 12:39:53 -0500

    default values for testing

--- BabaYaga/Modules/Environment.fs
+++ BabaYaga/Modules/Environment.fs
@@ -1,11 +1,28 @@
 ﻿module Modules.Environment
 
+let testDict = 
+    dict [
+        "AUTH_URL", "http://auth.com"
+        "CLIENT_ID", "123"
+        "CLIENT_SECRET", "123"
+        "AUDIENCE", "123"
+        "API_URL", "http://apiurl.com"
+        "SERVER", "123"
+        "PORT", "123"
+        "CHANNEL", "#123"
+        "NICK", "123"
+        "PASSWORD", "123"
+    ]
+
 let getEnvironmentVariables = 
-    let text = System.IO.File.ReadAllLines(".env")
-    text
-    |> Seq.map (fun x -> x.Split('='))
-    |> Seq.map (fun y -> y[0], y[1])
-    |> dict
+    if not <| System.IO.File.Exists(".env") then
+        testDict
+    else
+        let text = System.IO.File.ReadAllLines(".env")
+        text
+        |> Seq.map (fun x -> x.Split('='))
+        |> Seq.map (fun y -> y[0], y[1])
+        |> dict
 
 let (=?) left right = 
     System.String.Equals(left, right, System.StringComparison.CurrentCultureIgnoreCase)
